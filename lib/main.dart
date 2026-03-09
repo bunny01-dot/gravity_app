91d35b6092ccf76cb28439a6330742960b49feec
HAPPY <happy@local>
2026-03-10 00:51:37 +0530

    refactor: split resume and restart recovery helpers

--- lib/main.dart
+++ lib/main.dart
@@ -54,6 +54,7 @@ part 'main_app_shell.dart';
 part 'main_app_shell_runtime.dart';
 part 'main_app_shell_runtime_session.dart';
 part 'main_app_shell_runtime_watchdog.dart';
+part 'main_app_shell_runtime_recovery.dart';
 part 'main_recovery_widgets.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
