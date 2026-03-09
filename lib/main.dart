225ae6a4c7d8b059bb659efcee424cc05f1d2429
HAPPY <happy@local>
2026-03-10 00:54:55 +0530

    refactor: split lifecycle resume handlers from runtime

--- lib/main.dart
+++ lib/main.dart
@@ -55,6 +55,7 @@ part 'main_app_shell_runtime.dart';
 part 'main_app_shell_runtime_session.dart';
 part 'main_app_shell_runtime_watchdog.dart';
 part 'main_app_shell_runtime_recovery.dart';
+part 'main_app_shell_runtime_lifecycle.dart';
 part 'main_recovery_widgets.dart';
 
 final RouteObserver<ModalRoute<void>> routeObserver =
